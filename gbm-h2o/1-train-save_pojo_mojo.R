412736bbdf818906d38452e7473b3850336faa3c
szilard <szilard@users.noreply.github.com>
2017-05-23 13:59:57 -0700

    GBM h2o

---
+++ gbm-h2o/1-train-save_pojo_mojo.R
@@ -0,0 +1,15 @@
+
+library(h2o)
+h2o.init(nthreads = -1)
+
+dx_train <- h2o.importFile("https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv")
+
+system.time({
+  md <- h2o.gbm(x = 1:(ncol(dx_train)-1), y = ncol(dx_train), training_frame = dx_train, 
+                model_id = "airline_GBM",
+                ntrees = 100, max_depth = 10, learn_rate = 0.1, nbins = 100)
+})
+
+h2o.download_pojo(md, path = "./")
+h2o.download_mojo(md, path = "./")
+
