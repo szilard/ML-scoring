d8e04bfbde9a6ee6fae68588dd9cdbdaaa521217
szilard <szilard@users.noreply.github.com>
2017-05-23 20:38:43 -0700

    median

---
+++ h2o/5a-rest_speed_median.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+import numpy as np
+import requests
+import time
+
+df = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv")
+df = df.drop("dep_delayed_15min", 1)
+df = df.head(10000)
+
+url = "http://localhost:20000/predict"
+
+
+tm = np.zeros(10000)
+
+for index, row in df.iterrows(): 
+   params = row.to_json()
+   start  = time.time()
+   res = requests.post(url, data = params) 
+   tm[index] = time.time() - start
+  
+  
+print np.median(tm)*1000
+
+
+
+
+
