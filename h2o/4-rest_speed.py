46cd347480a25b340fe9e6427302bf85bbbb4e16
szilard <szilard@users.noreply.github.com>
2017-05-23 20:12:52 -0700

    mo

--- h2o/4-rest_speed.py
+++ h2o/4-rest_speed.py
@@ -6,12 +6,12 @@ import time
 
 df = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv")
 df = df.drop("dep_delayed_15min", 1)
-df = df.head(2000)
+df = df.head(10000)
 
 url = "http://localhost:20000/predict"
 
 
-tm = np.zeros(2000)
+tm = np.zeros(10000)
 
 for index, row in df.iterrows(): 
    params = row.to_json()
