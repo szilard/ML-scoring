99baf164defebd26493e76508d8129ea0aba32e8
szilard <szilard@users.noreply.github.com>
2017-05-23 19:59:32 -0700

    speed

--- h2o/4-rest_speed.py
+++ h2o/4-rest_speed.py
@@ -1,38 +1,28 @@
 
 import pandas as pd
+import numpy as np
 import requests
+import time
 
 df = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv")
 df = df.drop("dep_delayed_15min", 1)
-df0 = df.head(1000)
-dfN = df.head(2000).tail(1000)
-df1 = df.head(2001).tail(1)
+df = df.head(2000)
 
-url = 'http://localhost:20000/predict'
+url = "http://localhost:20000/predict"
 
 
-## python overhead
+tm = np.zeros(2000)
 
-%time for index, row in df0.iterrows(): \
+for index, row in df.iterrows(): 
    params = row.to_json()
+   start  = time.time()
+   res = requests.post(url, data = params) 
+   tm[index] = time.time() - start
+  
+  
+pd.DataFrame(tm).to_csv("tm.csv", header = False, index = False)
 
 
-## JVM warmup
 
-%time for index, row in df0.iterrows(): \
-   res = requests.post(url, data = row.to_json()) 
-   
-res.text
-
-
-## time it
-
-%time for index, row in dfN.iterrows(): \
-   res = requests.post(url, data = row.to_json()) 
-
-
-## time 1 req
-   
-%time requests.post(url, data = df1.to_json())
 
 
