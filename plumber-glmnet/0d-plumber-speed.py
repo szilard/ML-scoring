af067c50b8c2b333a29b0e415ee47ba37acafd9a
szilard <szilard@users.noreply.github.com>
2017-06-14 15:29:36 -0700

    w

--- plumber-glmnet/0d-plumber-speed.py
+++ plumber-glmnet/0d-plumber-speed.py
@@ -9,10 +9,10 @@ url = "http://localhost:8000/ff"
 tm = np.zeros(1000)
 
 for i in range(1000):
-   params = "{x1: " + str(np.random.choice(100,1)[0]) + "}"
+   params = {'x1': np.random.choice(100,1)[0]}
    start  = time.time()
    res = requests.get(url, params = params)
-   print(res.text)
+   ##print(res.text)
    tm[i] = time.time() - start
 
 print np.median(tm)*1000
