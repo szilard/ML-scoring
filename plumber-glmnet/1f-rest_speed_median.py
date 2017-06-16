0ee9ae071bd742eae472a3a39298180a81aab3e1
szilard <szilard@users.noreply.github.com>
2017-06-16 08:53:04 -0700

    w

--- plumber-glmnet/1f-rest_speed_median.py
+++ plumber-glmnet/1f-rest_speed_median.py
@@ -6,18 +6,20 @@ import time
 
 df = pd.read_csv("https://s3.amazonaws.com/benchm-ml--main/train-0.1m.csv")
 df = df.drop("dep_delayed_15min", 1)
-df = df.head(2000)
+df = df.head(500)
 
 url = "http://localhost:8000/score"
 
 
-tm = np.zeros(2000)
+tm = np.zeros(500)
 
 for index, row in df.iterrows(): 
    params = row.to_json()
+   print params
    start  = time.time()
-   res = requests.post(url, data = params) 
+   res = requests.get(url, data = params) 
    tm[index] = time.time() - start
+   print res.text
   
   
 print np.median(tm)*1000
